ed13b2e61885bac3636e43194054602e2b5d1f74
脑补菌 <30611253+NaobuOrg@users.noreply.github.com>
2018-01-18 10:57:04 +0800

    luhn算法校验信用卡号
    luhn算法校验信用卡号

--- functions/bank.php
+++ functions/bank.php
@@ -41,3 +41,70 @@ function getIIN($card_no = ''){
     }
     return '';
 }
+
+/**
+ * 基于luhn算法校验信用卡号合法性
+ * 该校验的过程：
+ * 1、从卡号最后一位数字开始，逆向将奇数位(1、3、5等等)相加。
+ * 2、从卡号最后一位数字开始，逆向将偶数位数字，先乘以2（如果乘积为两位数，则将其减去9），再求和。
+ * 3、将奇数位总和加上偶数位总和，结果应该可以被10整除。
+ * @param string $card_no
+ * @return bool
+ */
+function checkCardNo($card_no = ''){
+    $card_no = str_split(strrev($card_no));
+    $sum = 0;
+    foreach ($card_no as $k => $item){
+        if ($k%2){
+            // 奇数，卡号的偶数位
+            if ($item*2 > 9){
+                $sum += $item*2-9;
+            } else {
+                $sum += $item*2;
+            }
+        } else {
+            // 偶数，卡号的奇数位
+            $sum += $item;
+        }
+    }
+    if ($sum%10){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+/**
+ * 奶神思路
+ * @param string $card_no
+ * @return bool
+ */
+function checkCardNo2($card_no = ''){
+    $card_no = str_split(strrev($card_no));
+    $sum = 0;
+    foreach ($card_no as $k => $item){
+        $item *= (1+$k%2);//偶数位*2
+        $sum += ($item > 9 ? $item -9 : $item);
+    }
+    return $sum%10 == 0;
+}
+
+/**
+ * 大师思路
+ * @param string $card_no
+ * @return bool
+ */
+function checkCardNo3($card_no = ''){
+    $len = strlen($card_no);
+    $i = 0;
+    $code = array(
+        array(0,1,2,3,4,5,6,7,8,9),
+        array(0,2,4,6,8,1,3,5,7,9)
+    );
+    $sum = 0;
+    while ($len--){
+        $sum += $code[$i][$card_no{$len}];
+        $i = !$i;
+    }
+    return $sum % 10 == 0;
+}
