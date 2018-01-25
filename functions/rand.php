2462205eb9bbf5c075b4e1310bb5ec1e3d74f202
脑补菌 <30611253+NaobuOrg@users.noreply.github.com>
2018-01-25 18:59:04 +0800

    生成总和固定/指定个数/指定浮动范围的随机数

--- functions/rand.php
+++ functions/rand.php
@@ -38,6 +38,16 @@ function getGuid() {
 }
 
 /**
+ * 生成指定范围之间随机浮点数
+ * @param int $min 最小值，默认0
+ * @param int $max 最大值，默认1
+ * @return float|int 返回随机浮点数
+ */
+function randFloat($min=0, $max=1){
+    return $min + mt_rand()/mt_getrandmax() * ($max-$min);
+}
+
+/**
  * 生成总和固定且指定个数的随机数
  * @param float $sum 固定总和
  * @param int $num 指定随机次数
@@ -75,11 +85,38 @@ function randSplit($sum,$num){
 }
 
 /**
- * 生成指定范围之间随机浮点数
- * @param int $min 最小值，默认0
- * @param int $max 最大值，默认1
- * @return float|int 返回随机浮点数
+ * 生成总和固定/指定个数/指定浮动范围的随机数
+ * 依赖randSplit()随机函数
+ * @param float $sum 固定总和
+ * @param int $num 指定随机次数
+ * @param float $ratio 每个均值的上下浮动范围，取值范围0-1之间
+ * @return array 返回的随机数组
  */
-function randFloat($min=0, $max=1){
-    return $min + mt_rand()/mt_getrandmax() * ($max-$min);
+function randSplitForRatio($sum,$num,$ratio = 1.0){
+    $avg = $sum/$num;
+    $float = $avg * $ratio * 2;//$ratio为每个均值的上下浮动范围
+    $float_arr = randSplit($float,$num);//随机数组
+    $add_float = array();//随机增加数组
+    $reduce_float = array();//随机减少数组
+    // 对半拆分随机数组为一增一减数组
+    foreach ($float_arr as $k => $item){
+        $temp1 = round($item/2,2);
+        $temp2 = round($item - $temp1,2);
+        $add_float[] = $temp1;
+        $reduce_float[] = $temp2;
+    }
+    shuffle($reduce_float);//打乱其一数组顺序
+    $new_amount = 0;
+    $plan = array();
+    for ($i = 0; $i < $num; $i++){
+        if ($i !== ($num - 1)){
+            $temp1 = round($avg + $add_float[$i],2);
+            $temp2 = round($temp1 - $reduce_float[$i],2);
+            $plan[] = $temp2;
+            $new_amount += $temp2;
+        } else {
+            $plan[] = round($sum - $new_amount,2);
+        }
+    }
+    return $plan;
 }
